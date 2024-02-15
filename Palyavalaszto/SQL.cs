30169da9965fe9d85662e68f3bcda48353ff5bf6
HMarcell94 <136583369+HMarcell94@users.noreply.github.com>
2024-02-15 18:02:19 +0100

    .

---
+++ Palyavalaszto/SQL.cs
@@ -0,0 +1,22 @@
+﻿
+
+using DB_Module.SQL_Models;
+
+namespace DB_Module
+{
+    public class SQL : DbContext
+    {
+        public static string ConnectionString { get; set; }
+
+        public SQL() : base(new DbContextOptionsBuilder().UseSqlServer(ConnectionString).Options)
+        {
+        }
+
+        public SQL(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Tábla> Táblák { get; set; }
+        //DBSet-ek
+    }
+}
